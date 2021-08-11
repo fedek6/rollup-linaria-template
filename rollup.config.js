775c3c91ff6664bf6f1defee7b56c941abbefc19
Konrad Fedorczyk <contact@realhe.ro>
2021-08-11 13:02:46 +0200

    Add working demo

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+
+import typescript from 'rollup-plugin-typescript2';
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import commonjs from "@rollup/plugin-commonjs";
+import linaria from "@linaria/rollup";
+import css from "rollup-plugin-css-only";
+
+const packageJson = require("./package.json");
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: packageJson.main,
+      format: "cjs",
+      sourcemap: true,
+    },
+    {
+      file: packageJson.module,
+      format: "esm",
+      sourcemap: true
+    }
+  ],
+	plugins: [
+    peerDepsExternal(),
+    commonjs(),
+    linaria({
+      exclude: ["node_modules/**"],
+    }),
+    css({ output: "css/bundle.css" }),
+		typescript({ useTsconfigDeclarationDir: true, tsconfig: "./tsconfig.json" })
+	]
+}
---
+++ src/HelloWorld/HelloWorld.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import { css } from "@linaria/core";
+
+const header = css`
+  text-transform: uppercase;
+`;
+
+// Delete me
+export const HelloWorld = () => {
+  return <h1 className={header}>the snozzberries taste like snozzberries</h1>;
+};
