9196b05fcd1902dbc0932b4559a03a69afeeda8b
Konrad Fedorczyk <contact@realhe.ro>
2021-08-12 22:01:26 +0200

    Fix babel config for TS

--- src/HelloWorld/HelloWorld.tsx
+++ src/HelloWorld/HelloWorld.tsx
@@ -5,7 +5,10 @@ const header = css`
   text-transform: uppercase;
 `;
 
-// Delete me
-export const HelloWorld = () => {
-  return <h1 className={header}>the snozzberries taste like snozzberries</h1>;
+interface Props {
+  children: React.ReactNode;
+}
+
+export const HelloWorld = ({ children }: Props) => {
+  return <h1 className={header}>{ children }</h1>;
 };
