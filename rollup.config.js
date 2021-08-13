c5a37bfc57e26bdc13dc678c78c8824985b0b10f
Konrad Fedorczyk <contact@realhe.ro>
2021-08-13 11:06:45 +0200

    Fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 
-import typescript from 'rollup-plugin-typescript2';
+//import typescript from 'rollup-plugin-typescript2';
+import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import commonjs from "@rollup/plugin-commonjs";
 import linaria from "@linaria/rollup";
